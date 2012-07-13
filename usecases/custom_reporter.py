1f88bd72852b75c466e79670db06987ba5fd2789
ehsanf@gmail.com <ehsan@sdelements.com>
2012-07-13 01:04:20 -0400

    Cleaned up the directory structure a bit

--- usecases/custom_reporter.py
+++ usecases/custom_reporter.py
@@ -7,46 +7,40 @@
 # Proof of concept of a report generator
 # Piggybacks off Ehsan's Lint tool code
 
-import sys
-try:
-    import json
-except ImportError:
-    import json_compat as json
+import sys, os
+sys.path.append(os.path.split(os.path.split(os.path.abspath(__file__))[0])[0])
 
-
-from conf_mgr import config
-from commons import show_error
-from interactive_plugin import PlugInExperience
 import csv
 
-CSV_FILENAME = "output.csv"
+from sdelib.conf_mgr import config
+from sdelib.commons import show_error
+from sdelib.interactive_plugin import PlugInExperience
+from sdelib.apiclient import json
 
-"""
-    def read_config(self):
-        import ConfigParser
-        cnf = ConfigParser.ConfigParser()
-        cnf.read(self.config['cnf'])
-        self.ID = cnf.get('mysqld', 'server-id')
-"""
+CSV_FILENAME = "output.csv"
 
 def test_report(plugin):
      ret_err, ret_val = plugin.api.get_applications()
      if ret_err:
          return ret_err, ret_val
+
      csv_file = csv.writer(open(CSV_FILENAME, 'wb'))
      csv_file.writerow(['Application','Project','Task','Weakness','Status'])
      app_list = ret_val
+
      for app_ind in xrange(len(app_list)):
           app = app_list[app_ind]
           proj_ret_err, proj_ret_val = plugin.api.get_projects(app['id'])
           if proj_ret_err:
               return proj_ret_err, proj_ret_val
+
           proj_list = proj_ret_val
           for proj_ind in xrange(len(proj_list)):
               proj = proj_list[proj_ind]
               task_ret_err, task_ret_val = plugin.api.get_tasks(proj['id'])
               if task_ret_err:
                   return task_ret_err, task_ret_val
+
               task_list = task_ret_val
               for task_ind in xrange(len(task_list)):
                   task = task_list[task_ind]
@@ -57,15 +51,6 @@ def load():
     plugin = PlugInExperience(config)
     test_report(plugin)
     
-    #ret_err, ret_val = plugin.get_compiled_task_list()
-    #if ret_err:
-    #    show_error('Unexpected Error - code %s: %s' % (ret_err, ret_val))
-    #    sys.exit(1)
-        
-    #content = ret_val
-    #scanner = Scanner(config, content)
-    #scanner.scan()
-
 def main(argv):
     ret = config.parse_args(argv)
     if not ret:
