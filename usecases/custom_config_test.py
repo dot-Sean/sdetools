4a3defad6f5ba99f284d8e16b8a539214740079d
ehsanf@gmail.com <ehsan@sdelements.com>
2012-07-24 16:22:30 -0400

    Added support for Customization of Configuration

---
+++ usecases/custom_config_test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+#
+# Version 0.5
+# Ehsan Foroughi
+# Copyright SDElements Inc
+#
+
+import sys, os
+sys.path.append(os.path.split(os.path.split(os.path.abspath(__file__))[0])[0])
+
+from sdelib.conf_mgr import Config
+
+def main(argv):
+    config = Config()
+    config.add_custom_option('mytest', 'This is my test option', 'm')
+
+    ret = config.parse_args(argv)
+    if not ret:
+        sys.exit(1)
+    
+    print 'Tests you can do'
+    print '1. Run with -h to see the options'
+    print '2. Run with -m <value> to set the option'
+    print '3. Add myvar = <value> to config file of your choice'
+    print
+    print 'RESULT: The value of mytest config is %s' % (config['mytest'])
+
+if __name__ == "__main__":
+    main(sys.argv)
+
