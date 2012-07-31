0ce440e64d5181e471288e17b6a37958902b67ea
Geoff Whittington <geoff@sdelements.com>
2012-07-31 12:02:32 -0400

    1 note is now only created for each affected task

--- veracode_integration/mapping/base_integrator.py
+++ veracode_integration/mapping/base_integrator.py
@@ -13,6 +13,7 @@ from sdelib.interactive_plugin import PlugInExperience
 class BaseIntegrator:
     def __init__(self, config):
         self.findings = []
+        self.phase_exceptions = ['testing']
         self.mapping = {}
         self.report_id = ""
         self.config = config
@@ -22,7 +23,11 @@ class BaseIntegrator:
         csv_mapping = {}
         mappingReader = csv.reader(open(csv_file),delimiter=',',quotechar='"')
         for row in mappingReader:
-            csv_mapping[row[1]]=row[0]
+            tasks = []
+            if (csv_mapping.has_key(row[5])):
+               tasks = csv_mapping[row[5]]
+            tasks.append(row[3])
+            csv_mapping[row[5]]=tasks
         self.mapping = csv_mapping
 
     def get_findings(self):
@@ -34,6 +39,30 @@ class BaseIntegrator:
     def output_mapping(self):
         print self.mapping
 
+    def unique_findings(self):
+        unique_findings = {}
+        unique_findings['nomap'] = []
+        for finding in self.get_findings():
+            mapped_tasks = self.lookup_task( finding['cweid'] )
+            file_name = ''
+            if finding.has_key('source'):
+                file_name=finding['source']
+            if ( mapped_tasks == None or len(mapped_tasks) == 0):
+                unique_findings['nomap'].append(finding['cweid'])
+                continue
+
+            for mapped_task_id in mapped_tasks:
+                flaw = {}
+                if(unique_findings.has_key(mapped_task_id)):
+                    flaw = unique_findings[mapped_task_id]
+                else:
+                    flaw = {}
+                    flaw['count'] = 0
+                flaw['count'] += 1
+                flaw['cweid'] =  finding['cweid']
+                unique_findings[mapped_task_id]=flaw
+        return unique_findings
+
     def output_findings(self):
         for item in self.findings:
             print '%5s,%5s,%5s,%s' % (item['issueid'], item['cweid'], item['categoryid'],item['description'][:120])
@@ -42,6 +71,19 @@ class BaseIntegrator:
         if(self.mapping.has_key(cwe_id)):
             return self.mapping[cwe_id]
         return None
+
+    def task_exists(self, needle_task_id, haystack_tasks):
+        for task in haystack_tasks:
+            task_id = re.search('(\d+)-[^\d]+(\d+)', task['id']).group(2)
+            if(task_id==needle_task_id):
+                return True
+        return False
+
+    def mapping_contains_task(self, needle_task_id):
+        for task_id in self.mapping.values():
+            if(needle_task_id==task_id):
+                return True
+        return False
 
     def log_message (self, severity, message):
         now = datetime.now().isoformat(' ')
@@ -63,23 +105,35 @@ class BaseIntegrator:
 
         self.log_message(severityMsg, "Integration underway: %s" % (self.report_id))
 
-        for finding in self.get_findings():
-            mapped_task_id = self.lookup_task( finding['cweid'] )
-            file_name = ''
-            if finding.has_key('source'):
-                file_name=finding['source']
-            if ( mapped_task_id == None or mapped_task_id == ''):
-                self.log_message(severityWarn, "Could not map finding (cweid=%s,source=%s)" % (finding['cweid'], file_name))
-                stats_missing_maps += 1
+        task_list = []
+        tasks_found = []
+        try:
+            task_list = self.plugin.get_task_list()
+        except APIError, e:
+            self.log_message(severityError,"Could not get task list for %s - Reason: %s" % (self.config['project'], str(e)))
+            stats_errors += 1
+
+        unique_findings = self.unique_findings()
+        missing_cwe_map = unique_findings['nomap']
+        del unique_findings['nomap']
+
+        stats_total_flaws = 0
+
+        for task_id in unique_findings.iterkeys():
+            finding = unique_findings[task_id]
+            stats_total_flaws += finding['count']
+
+            if (not self.task_exists( task_id, task_list)):
+                self.log_message(severityMsg, "Task %s was not found in the project task list, skipping." % (task_id))
                 continue
 
-            task_id = "T%s" % (mapped_task_id)
+            task_id = "T%s" % (task_id)
 
+            file_name = ''
             description  = "Analysis: %s\n\n" % (self.report_id)
             description += "CWE: http://cwe.mitre.org/data/definitions/%s.html\n\n" % (finding['cweid'])
-            description += "Detail: %s\n" % (finding['description'][:255])
-            if (finding.has_key('line')):
-                description += "Line: %s" % (finding['line'])
+            description += "Flaws found: %d" % (finding['count'])
+
 
             msg = "%s/%s/%s (cweid=%s,source=%s)" % (self.config['application'], self.config['project'], task_id, finding['cweid'], file_name)
 
@@ -95,18 +149,20 @@ class BaseIntegrator:
                 self.log_message(severityTestRun, "TODO note added to %s" % (msg))
                 stats_subtasks_added += 1
 
-        task_list = []
-        try:
-            task_list = self.plugin.get_task_list()
-        except APIError, e:
-            self.log_message(severityError,"Could not get task list for %s - Reason: %s" % (self.config['project'], str(e)))
-            stats_errors += 1
+        stats_unaffected_tasks=0
 
         for task in task_list:
+            if(task['phase'] in self.phase_exceptions):
+                continue
+
             task_id = re.search('(\d+)-[^\d]+(\d+)', task['id']).group(2)
+            if(unique_findings.has_key(task_id)):
+                continue
+            
+            stats_unaffected_tasks+=1
 
             msg = "%s/%s/%s" % (self.config['application'], self.config['project'], task_id)
-            description = "Analysis: %s\n\nProcess completed" % (self.report_id)
+            description = "Analysis: %s\n\nProcess completed with 0 flaws found." % (self.report_id)
             file_name = ''
 
             if commit:
@@ -121,10 +177,12 @@ class BaseIntegrator:
                 self.log_message(severityTestRun, "Marked %s as DONE" % (msg))
                 stats_tasks_affected += 1
 
+        self.log_message(severityMsg, "These CWEs were not mapped: "+ ",".join(missing_cwe_map))
         self.log_message(severityMsg, "---------------------------------------------------------")
-        self.log_message(severityMsg, "%d subtasks created from %d flaws."%(stats_subtasks_added, len(self.get_findings())))
-        self.log_message(severityMsg, "%d subtasks could not be created due to unknown mapping." %(stats_missing_maps))
+        self.log_message(severityMsg, "%d subtasks created from %d flaws."%(stats_subtasks_added, stats_total_flaws))
+        self.log_message(severityMsg, "%d flaws could not be mapped." %(len(missing_cwe_map)))
         self.log_message(severityMsg, "%d errors encountered." % (stats_errors))
+        self.log_message(severityMsg, "%d project tasks had 0 flaws." %(stats_unaffected_tasks))
         self.log_message(severityMsg, "---------------------------------------------------------")
         self.log_message(severityMsg, "Completed")
 
