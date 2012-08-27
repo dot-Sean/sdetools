489e0c8b6003654a91482150413c1cde19e4e39e
Ramanan Sivaranjan <ramanan@funkaoshi.com>
2012-08-27 11:48:34 -0400

    Cleanup Veracode integration.
    These are mostly style fixes: making this a bit more PEP8 compliant,
    using defaultdict to simplify some code, using python idioms for things
    like checking if a list is empty, etc.
    
    The unit test still passes, but it's a bit brittle. It fails when run
    against Melbourne. We might want to better document what needs to
    happen for it to work.

--- veracode_integration/base_integrator.py
+++ veracode_integration/base_integrator.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import sys, os
+import collections
 import csv
 import re
 from datetime import datetime
@@ -17,12 +18,16 @@ class IntegrationError(Error):
     pass
 
 class IntegrationResult:
-    def __init__(self):
-        self.import_datetime=''
-        self.total_unaffected_tasks=0
-        self.total_affected_tasks=0
-        self.error_count=0
-        self.error_cwes_unmapped=0
+    def __init__(self, import_datetime, affected_tasks, noflaw_tasks,
+            error_count, error_cwes_unmapped):
+        self.import_datetime = import_datetime
+        self.affected_tasks = affected_tasks
+        self.noflaw_tasks = noflaw_tasks
+        self.error_count = error_count
+        self.error_cwes_unmapped = error_cwes_unmapped
+        # TODO: Do we need / use these?
+        # self.total_affected_tasks = 0
+        # self.total_unaffected_tasks = 0
 
 class BaseIntegrator:
     def __init__(self, config):
@@ -35,9 +40,12 @@ class BaseIntegrator:
         self._init_config()
 
     def _init_config(self):
-        self.config.add_custom_option("mapping_file", "Task ID -> CWE mapping in XML format", "m")
-        self.config.add_custom_option("logging", "Logging level: on | off", "l", "off")
-        self.config.add_custom_option("trial_run", "Trial run only: 'true' or 'false' (default)", "t", "false")
+        self.config.add_custom_option("mapping_file",
+                "Task ID -> CWE mapping in XML format", "m")
+        self.config.add_custom_option("logging",
+                "Logging level: on | off", "l", "off")
+        self.config.add_custom_option("trial_run",
+                "Trial run only: 'true' or 'false' (default)", "t", "false")
 
     def parse_args(self, argv):
         ret = self.config.parse_args(argv)
@@ -46,6 +54,7 @@ class BaseIntegrator:
         self.init_plugin()
 
     def init_plugin(self):
+        # TODO: Should this be config, or self.config?
         self.plugin = PlugInExperience(config)
 
     def load_mapping_from_xml(self):
@@ -54,23 +63,19 @@ class BaseIntegrator:
         except KeyError, ke:
             raise IntegrationError("Missing configuration option 'mapping_file'")
         except Exception, e:
-            raise IntegrationError("An error occured opening mapping file '%s'" % config['mapping_file'])
+            raise IntegrationError("An error occurred opening mapping file '%s'" % config['mapping_file'])
 
-        cwe_mapping = {}
+        cwe_mapping = collections.defaultdict(list)
         for task in base.getElementsByTagName('task'):
             for cwe in task.getElementsByTagName('cwe'):
-                 tasks = []
-                 if (cwe_mapping.has_key(cwe.attributes['id'].value)):
-                     tasks = cwe_mapping[cwe.attributes['id'].value]
-                 tasks.append(task.attributes['id'].value)
-                 cwe_mapping[cwe.attributes['id'].value] = tasks
+                cwe_id = cwe.attributes['id'].value
+                cwe_mapping[cwe_id].append(task.attributes['id'].value)
 
         self.mapping = cwe_mapping
-        if(len(self.mapping) == 0):
+        if not self.mapping:
             raise IntegrationError("No mapping was found in file '%s'" % config['mapping_file'])
 
     def load_mapping_from_csv(self):
-        csv_mapping = {}
         try:
             mapping_reader = csv.reader(open(csv_file),delimiter=',',quotechar='"')
         except KeyError, ke:
@@ -78,15 +83,18 @@ class BaseIntegrator:
         except Exception, e:
              raise IntegrationError("An error occured opening mapping file '%s'" % csv_file)
 
+        csv_mapping = collections.defaultdict(list)
         for row in mapping_reader:
-            tasks = []
-            if (csv_mapping.has_key(row[5])):
-               tasks = csv_mapping[row[5]]
-            tasks.append(row[3])
-            csv_mapping[row[5]]=tasks
+            # TODO: what is row[5]? what is row[3]?
+            key = row[5]
+            csv_mapping[key].append(row[3])
         self.mapping = csv_mapping
 
     def get_findings(self):
+        # TODO: Do we really need get_findings() and generate_findings(). The
+        #       later seems to be what we want. I'm also not a fan of get_xyz
+        #       methods with side effects. Do we want to rebuild the findings
+        #       everytime this is called?
         self.generate_findings()
         return self.findings
 
@@ -104,27 +112,21 @@ class BaseIntegrator:
             flaw[cwes]
             flaw[related_tasks]
         """
-        unique_findings = {}
-        unique_findings['nomap'] = []
+        unique_findings = {'nomap': []}
         for finding in self.get_findings():
-            mapped_tasks = self.lookup_task( finding['cweid'] )
-            file_name = ''
-            if finding.has_key('source'):
-                file_name=finding['source']
-            if ( mapped_tasks == None or len(mapped_tasks) == 0):
-                unique_findings['nomap'].append(finding['cweid'])
+            cwe_id = finding['cweid']
+            mapped_tasks = self.lookup_task(cwe_id)
+            if not mapped_tasks:
+                unique_findings['nomap'].append(cwe_id)
                 continue
-
-            flaws = {}
             for mapped_task_id in mapped_tasks:
-                if(unique_findings.has_key(mapped_task_id)):
+                if unique_findings.has_key(mapped_task_id):
                     flaws = unique_findings[mapped_task_id]
                 else:
-                    flaws = {}
-                    flaws['cwes'] = []
-                flaws['cwes'].append(finding['cweid'])
+                    flaws = {'cwes': []}
+                flaws['cwes'].append(cwe_id)
                 flaws['related_tasks'] = mapped_tasks
-                unique_findings[mapped_task_id]=flaws
+                unique_findings[mapped_task_id] = flaws
         return unique_findings
 
     def output_findings(self):
@@ -132,32 +134,32 @@ class BaseIntegrator:
             print '%5s,%5s,%5s,%s' % (item['issueid'], item['cweid'], item['categoryid'],item['description'][:120])
 
     def lookup_task(self, cwe_id):
-        if(self.mapping.has_key(cwe_id)):
+        if self.mapping.has_key(cwe_id):
             return self.mapping[cwe_id]
-        if(self.mapping.has_key('*')):
+        if self.mapping.has_key('*'):
             return self.mapping['*']
         return None
 
     def task_exists(self, needle_task_id, haystack_tasks):
         for task in haystack_tasks:
             task_id = re.search('(\d+)-[^\d]+(\d+)', task['id']).group(2)
-            if(task_id==needle_task_id):
+            if task_id == needle_task_id:
                 return True
         return False
 
     def mapping_contains_task(self, needle_task_id):
         for task_id in self.mapping.values():
-            if(needle_task_id==task_id):
+            if needle_task_id == task_id:
                 return True
         return False
 
     def log_message (self, severity, message):
-        if(config['logging'] == 'on'):
+        # TODO: We should use python's built in logger.
+        if config['logging'] == 'on':
             now = datetime.now().isoformat(' ')
             print "%s - %s - %s" % (now, severity, message)
 
     def import_findings(self):
-
         commit = (config['trial_run'] != 'true')
 
         stats_subtasks_added = 0
@@ -166,15 +168,17 @@ class BaseIntegrator:
         stats_missing_maps = 0
         stats_total_skips = 0
         stats_total_skips_findings = 0
-
         import_datetime = datetime.now().isoformat(' ')
 
         self.generate_findings()
 
+        # TODO: There must be a better place to declare these. When a sub
+        #       class wants to log a message it would have to redeclare these
+        #       values.
         severityError = "ERROR"
         severityWarn = "WARN"
         severityMsg = "INFO"
-        severityTestRun = "TEST_RUN"        
+        severityTestRun = "TEST_RUN"
 
         self.log_message(severityMsg, "Integration underway for: %s" % (self.report_id))
         self.log_message(severityMsg, "Mapped application/project: %s/%s" % (self.config['application'],self.config['project']))
@@ -185,7 +189,7 @@ class BaseIntegrator:
         try:
             task_list = self.plugin.get_task_list()
         except APIError, e:
-            self.log_message(severityError,"Could not get task list for %s - Reason: %s" % (self.config['project'], str(e)))
+            self.log_message(severityError, "Could not get task list for %s - Reason: %s" % (self.config['project'], str(e)))
             stats_api_errors += 1
 
         unique_findings = self.unique_findings()
@@ -197,15 +201,15 @@ class BaseIntegrator:
         for task_id in task_ids:
             finding = unique_findings[task_id]
 
-            if (not self.task_exists( task_id, task_list)):
+            if not self.task_exists( task_id, task_list):
                 mapped_tasks = self.lookup_task("*")
-                if(mapped_tasks <> None and len(mapped_tasks)>0):
+                if mapped_tasks:
                     new_task_id = mapped_tasks[0] # use the first one
-                    if (task_id <> new_task_id):
+                    if task_id != new_task_id:
                         self.log_message(severityWarn, "Task %s was not found in the project, mapping it to the default task %s." % (task_id, new_task_id))
-                        task_id=new_task_id
+                        task_id = new_task_id
 
-            if (not self.task_exists( task_id, task_list)):
+            if not self.task_exists(task_id, task_list):
                 self.log_message(severityError, "Task %s was not found in the project, skipping %d findings." % (task_id, len(finding['cwes'])))
                 stats_total_skips += 1
                 stats_total_skips_findings += len(finding['cwes'])
@@ -213,25 +217,24 @@ class BaseIntegrator:
 
             task_id = "T%s" % (task_id)
 
-            file_name = ''
             description  = "Update from external analysis: %s\n" % (self.report_id)
             description += "Imported on: %s\n\n" % (import_datetime)
-
-            if (len(finding['cwes']) > 0):
+            if finding['cwes']:
                 description += "Analysis did not complete successfully: %d flaws were identified related to this task. " % len(finding['cwes'])
                 description += "The flaws are associated to the following common weakness:\n"
             else:
                 description += "Analysis did not complete successfully: 1 flaw was identified that relates to this task. "
                 description += "The flaw is associated to the common weakness:\n"
- 
+
             for cwe in set(finding['cwes']):
-                description += "http://cwe.mitre.org/data/definitions/"+cwe+"\n"
+                description += "http://cwe.mitre.org/data/definitions/" + cwe + "\n"
 
             description = description[:800]
 
             msg = "%s" % task_id
 
             if commit:
+                file_name = ''  # Currently required to indicate note was created via API.
                 try:
                     api_result = self.plugin.add_note(task_id, description, file_name, "TODO")
                     self.log_message(severityMsg, "TODO note added to %s" % (msg))
@@ -252,25 +255,20 @@ class BaseIntegrator:
             if(task['phase'] in self.phase_exceptions):
                 stats_test_tasks+=1
                 continue
-
             task_id = re.search('(\d+)-[^\d]+(\d+)', task['id']).group(2)
             if(unique_findings.has_key(task_id)):
                 affected_tasks.append(int(task_id))
                 continue
-
             noflaw_tasks.append(int(task_id))
-
         noflaw_tasks = sorted(noflaw_tasks)
 
         for task_id in noflaw_tasks:
-
             msg = "T%s" % task_id
             description  = "Update from external analysis: %s\n" % (self.report_id)
             description += "Imported on: %s\n\n" % (import_datetime)
             description += "Analysis completed successfully: no flaws could be identified for this task."
 
-            file_name = ''
-
+            file_name = ''  # Currently required to indicate note was created via API.
             if commit:
                 try:
                     self.plugin.add_note("T%s" % (task_id), description, file_name, "DONE")
@@ -284,32 +282,31 @@ class BaseIntegrator:
                 stats_noflaw_notes_added += 1
 
         self.log_message(severityMsg, "---------------------------------------------------------")
-        if (len(missing_cwe_map) > 0):
+        if missing_cwe_map:
             self.log_message(severityError, "These CWEs could not be mapped: "+ ",".join(missing_cwe_map))
             self.log_message(severityError, "%d total flaws could not be mapped." %(len(missing_cwe_map)))
         else:
              self.log_message(severityMsg, "All CWEs successfully mapped to a task.")
         self.log_message(severityMsg, "%d subtasks created from %d flaws."%(stats_subtasks_added, len(self.findings)))
         self.log_message(severityMsg, "%d/%d project tasks had 0 flaws." %(len(noflaw_tasks),len(task_list)-(stats_test_tasks)))
-        if (stats_total_skips > 0):
+        if stats_total_skips:
             self.log_message(severityError, "%d flaws were mapped to %d tasks not found in the project. Skipped"%(stats_total_skips_findings,stats_total_skips))
         self.log_message(severityMsg, "%d total api errors encountered." % (stats_api_errors))
         self.log_message(severityMsg, "---------------------------------------------------------")
         self.log_message(severityMsg, "Completed")
 
-        result = IntegrationResult()
-        result.import_datetime=import_datetime
-        result.affected_tasks=affected_tasks
-        result.noflaw_tasks=noflaw_tasks
-        result.error_count=stats_api_errors
-        result.error_cwes_unmapped=len(missing_cwe_map)
-        return result
+        return IntegrationResult(import_datetime=import_datetime,
+                                 affected_tasks=affected_tasks,
+                                 noflaw_tasks=noflaw_tasks,
+                                 error_count=stats_api_errors,
+                                 error_cwes_unmapped=len(missing_cwe_map))
 
 def main(argv):
     base = BaseIntegrator(config)
     try:
         base.parse_args(argv)
     except:
+        # TODO: We should catch an actual exception, or remove this code.
         sys.exit(1)
 
     base.load_mapping_from_xml()
--- veracode_integration/tests/veracode_test.py
+++ veracode_integration/tests/veracode_test.py
@@ -15,54 +15,39 @@ class TestVeracode(unittest.TestCase):
           self.integrator = VeracodeIntegrator(config)
           config.parse_config_file(CONF_FILE_LOCATION)
           self.integrator.init_plugin()
-          self.assertTrue(True)
 
      def test_import_findings(self):
+         self.assertTrue(config['trial_run'] != 'true')
+         task_list = self.integrator.plugin.get_task_list()
+         total_nontest_tasks = len([task for task in task_list
+                                   if task['phase'] not in ['testing']])
+         self.integrator.load_mapping_from_xml()
+         self.integrator.parse()
+         findings = self.integrator.get_findings()
+         base = minidom.parse(config['report_xml'])
+         xml_findings_count = len(base.getElementsByTagName('flaw'))
+         self.assertTrue(xml_findings_count == len(findings))
 
-          self.assertTrue (config['trial_run'] != 'true')
+         result = self.integrator.import_findings()
+         self.assertTrue(result.error_count == 0)
+         all_tasks_sanity_check = (len(result.noflaw_tasks) + len(result.affected_tasks)) == total_nontest_tasks
+         self.assertTrue(all_tasks_sanity_check)
+         self.assertTrue(result.error_cwes_unmapped == 0)
 
-          try:
-              task_list = self.integrator.plugin.get_task_list()
-          except:
-              self.assertTrue(False)
+         for task_id in result.noflaw_tasks:
+             api_result = self.integrator.plugin.get_notes("T%d" % task_id)
+             notes = api_result['notes']
+             last_note = notes[len(notes)-1]
+             self.assertTrue(last_note['text'].find(result.import_datetime) > -1)
+             self.assertTrue(last_note['status'] == "DONE")
 
-          total_nontest_tasks = 0
-          for task in task_list:
-              if(task['phase'] in ['testing']):
-                  continue
-              total_nontest_tasks += 1
+         for task_id in result.affected_tasks:
+             api_result = self.integrator.plugin.get_notes("T%d" % task_id)
+             notes = api_result['notes']
+             last_note = notes[len(notes)-1]
+             self.assertTrue(last_note['text'].find(result.import_datetime) > -1)
+             self.assertTrue(last_note['status'] == "TODO")
 
-          self.integrator.load_mapping_from_xml()
-          self.integrator.parse()
-          findings = self.integrator.get_findings()
-          xml_findings_count=0
-          try:
-              base = minidom.parse(config['report_xml'])
-              xml_findings_count = len(base.getElementsByTagName('flaw'))
-          except:
-              self.assertTrue(False)
-          self.assertTrue( xml_findings_count == len(findings)  )
-
-          result = self.integrator.import_findings()
-          self.assertTrue(result.error_count==0)
-          all_tasks_sanity_check = (len(result.noflaw_tasks)+len(result.affected_tasks)) == total_nontest_tasks
-          self.assertTrue(all_tasks_sanity_check)
-          self.assertTrue(result.error_cwes_unmapped==0)
-
-          for task_id in result.noflaw_tasks:
-              api_result = self.integrator.plugin.get_notes("T%d"%task_id)
-              notes = api_result['notes']
-              last_note = notes[len(notes)-1]
-              self.assertTrue(last_note['text'].find(result.import_datetime) > -1 )
-              self.assertTrue(last_note['status'] == "DONE")
-
-          for task_id in result.affected_tasks:
-              api_result = self.integrator.plugin.get_notes("T%d"%task_id)
-              notes = api_result['notes']
-              last_note = notes[len(notes)-1]
-              self.assertTrue(last_note['text'].find(result.import_datetime) > -1 )
-              self.assertTrue(last_note['status'] == "TODO")
-    
 if __name__ == "__main__":
     unittest.main()
 
