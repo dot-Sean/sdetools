3be05e8686eecef20236da1f680b1dce3fb8af5d
Ehsan Foroughi <ehsanf@gmail.com>
2012-10-30 15:34:16 -0400

    Fixing the windows compiler

--- setup.py
+++ setup.py
@@ -8,7 +8,18 @@ except ImportError:
 
 from distutils.core import setup
 
+import modules
+options = {
+    'py2exe': {
+        'includes':[]
+    }
+}
+
+for mod_name in modules.__all__:
+    options['py2exe']['includes'].append('modules.%s' % mod_name)
+
 setup(
     console=['sde.py'],
     zipfile=None,
+    options=options
     )
