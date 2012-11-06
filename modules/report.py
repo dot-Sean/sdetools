04560350bf2036c373f364e5ab60f82fdf91e081
ehsanf@gmail.com <Ehsan Foroughi>
2012-11-06 13:38:29 -0500

    fixed indent

--- modules/report.py
+++ modules/report.py
@@ -18,22 +18,22 @@ class Command(BaseCommand):
         self.plugin = PlugInExperience(self.config)
 
     def test_report(self, plugin):
-         app_list = plugin.api.get_applications()
+        app_list = plugin.api.get_applications()
 
-         csv_file = csv.writer(open(CSV_FILENAME, 'wb'))
-         csv_file.writerow(['Application','Project','Task','Weakness','Status'])
+        csv_file = csv.writer(open(CSV_FILENAME, 'wb'))
+        csv_file.writerow(['Application','Project','Task','Weakness','Status'])
 
-         for app_ind in xrange(len(app_list)):
-              app = app_list[app_ind]
-              proj_list = plugin.api.get_projects(app['id'])
+        for app_ind in xrange(len(app_list)):
+             app = app_list[app_ind]
+             proj_list = plugin.api.get_projects(app['id'])
 
-              for proj_ind in xrange(len(proj_list)):
-                  proj = proj_list[proj_ind]
-                  task_list = plugin.api.get_tasks(proj['id'])
+             for proj_ind in xrange(len(proj_list)):
+                 proj = proj_list[proj_ind]
+                 task_list = plugin.api.get_tasks(proj['id'])
 
-                  for task_ind in xrange(len(task_list)):
-                      task = task_list[task_ind]
-                      csv_file.writerow([app['name'],proj['name'],task['title'],task['weakness']['title'],task['status']])
+                 for task_ind in xrange(len(task_list)):
+                     task = task_list[task_ind]
+                     csv_file.writerow([app['name'],proj['name'],task['title'],task['weakness']['title'],task['status']])
 
     def handle(self):
         self.test_report(self.plugin)
