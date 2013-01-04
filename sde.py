a98c3c4f368645386294fb0482da1eca8579e1e5
Ehsan Foroughi <ehsan@gmail.com>
2013-01-04 11:37:50 -0500

    Organizing modules. Goal: Getting to an importable module

--- sde.py
+++ sde.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python
 import sys
 import os
-import getopt
-import logging
 
 if sys.platform.startswith("win"):
     current_file = sys.argv[0]
@@ -13,35 +11,9 @@ BASE_PATH = os.path.split(os.path.abspath(current_file))[0]
 from sdelib import commons
 commons.base_path = BASE_PATH
 
-from sdelib import conf_mgr
-import modules
-
-def load_modules():
-    command = {}
-
-    for mod_name in modules.__all__:
-        if mod_name.startswith('_'):
-            continue
-        try:
-            mod = __import__('modules.' + mod_name)
-        except ImportError:
-            logging.exception('Exception in importing module %s' % (mod_name))
-            raise commons.UsageError('Unable to import module %s' % (mod_name))
-        mod = getattr(mod, mod_name)
-        if not hasattr(mod, 'Command'):
-            raise commons.UsageError('Module missing Command class: %s' % (mod_name))
-        cmd_cls = mod.Command
-        if not hasattr(cmd_cls, 'name'):
-            cmd_cls.name = mod_name
-        if not hasattr(cmd_cls, 'help'):
-            raise commons.UsageError('Missing help string for module %s' % (cmd_cls.name))
-        command[cmd_cls.name] = cmd_cls
-
-    return command
+from sdelib import mod_mgr
 
 def main(argv):
-    command = load_modules()
-
     if len(argv) < 2:
         commons.show_error("Missing command", usage_hint=True)
         return False
@@ -56,42 +28,16 @@ def main(argv):
         commons.show_error("Missing command", usage_hint=True)
         return False
 
-    if curr_cmd_name not in command:
-        commons.show_error("Command not found: %s" % (curr_cmd_name), usage_hint=True)
-        return False
-
-    curr_cmd = command[curr_cmd_name]
-
-    config = conf_mgr.Config(command)
-
-    cmd_inst = curr_cmd(config, argv[2:])
     try:
-        cmd_inst.configure()
-
-        ret_status = cmd_inst.parse_args()
+        exit_stat = mod_mgr.run_command(curr_cmd_name, argv[2:])
     except commons.UsageError, e:
-        commons.show_error(str(e))
+        commons.show_error(str(e), usage_hint=True)
         return False
-
-    if not ret_status:
-        return False
-
-    cmd_inst.args = config.args
-    try:
-        cmd_inst.process_args()
     except commons.Error, e:
         commons.show_error(str(e))
         return False
 
-    try:
-        ret_status = cmd_inst.handle()
-    except commons.Error, e:
-        commons.show_error(str(e))
-        return False
-
-    if ret_status is None:
-        ret_status = True
-    return ret_status
+    return exit_stat
 
 if __name__ == "__main__":
     exit_stat = main(sys.argv)
---
+++ sdelib/mod_mgr.py
@@ -0,0 +1,58 @@
+import logging
+
+import conf_mgr
+import commons
+
+import modules
+
+def load_modules():
+    command = {}
+
+    for mod_name in modules.__all__:
+        if mod_name.startswith('_'):
+            continue
+        try:
+            mod = __import__('modules.' + mod_name)
+        except ImportError:
+            logging.exception('Exception in importing module %s' % (mod_name))
+            raise commons.UsageError('Unable to import module %s' % (mod_name))
+        mod = getattr(mod, mod_name)
+        if not hasattr(mod, 'Command'):
+            raise commons.UsageError('Module missing Command class: %s' % (mod_name))
+        cmd_cls = mod.Command
+        if not hasattr(cmd_cls, 'name'):
+            cmd_cls.name = mod_name
+        if not hasattr(cmd_cls, 'help'):
+            raise commons.UsageError('Missing help string for module %s' % (cmd_cls.name))
+        command[cmd_cls.name] = cmd_cls
+
+    return command
+
+def run_command(cmd_name, args):
+    command = load_modules()
+
+    if cmd_name not in command:
+        raise commons.UsageError("Command not found: %s" % (cmd_name))
+
+    curr_cmd = command[cmd_name]
+
+    config = conf_mgr.Config(command)
+
+    cmd_inst = curr_cmd(config, args)
+
+    cmd_inst.configure()
+
+    ret_status = cmd_inst.parse_args()
+
+    if not ret_status:
+        return False
+
+    cmd_inst.args = config.args
+    cmd_inst.process_args()
+
+    ret_status = cmd_inst.handle()
+
+    if ret_status is None:
+        ret_status = True
+    return ret_status
+
