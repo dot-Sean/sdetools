d7f5e0742e68865351850de1ecc527eaaddad669
Sam Bull <sam@pocketuniverse.ca>
2013-01-26 01:20:50 -0500

    Make py2exe optional in setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,24 @@
-import sys
 import os
+
+from distutils.core import setup
+
+from sdetools.sdelib import commons
+
+options = {}
 
 try:
     import py2exe
 except ImportError:
-    print "Error: Missing py2exe package. Use this for windows compilation only."
-    sys.exit(1)
+    print "Warning: Missing py2exe package. Use this for windows compilation only."
+else:
+    from sdetools import modules
+    options['py2exe'] = {
+            'includes':[]
+        }
 
-from distutils.core import setup
+    for mod_name in modules.__all__:
+        options['py2exe']['includes'].append('sdetools.modules.%s' % mod_name)
 
-from sdetools import modules
-from sdetools.sdelib import commons
-
-options = {
-    'py2exe': {
-        'includes':[]
-    }
-}
-
-for mod_name in modules.__all__:
-    options['py2exe']['includes'].append('sdetools.modules.%s' % mod_name)
 
 static_files = []
 root_path = os.path.split(commons.base_path)[0]
