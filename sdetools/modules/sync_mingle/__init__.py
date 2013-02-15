f0f3900eba62246edba0340db97cbb8671e178bc
Ehsan Foroughi <ehsan@gmail.com>
2013-02-15 13:01:12 -0500

    removing unused code

--- sdetools/modules/sync_mingle/__init__.py
+++ sdetools/modules/sync_mingle/__init__.py
@@ -1,8 +1,5 @@
 from sdetools.sdelib.cmd import BaseCommand
 from sdetools.modules.sync_mingle.mingle_plugin import MingleConnector, MingleAPIBase
-from sdetools.alm_integration.alm_plugin_base import AlmException
-from sdetools.sdelib.interactive_plugin import PluginError
-
 
 class Command(BaseCommand):
     help = 'Mingle <-> SDE sync utility.'
