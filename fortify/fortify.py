a90c9ba700a8f4b6b486b9558e25f73266084971
Ehsan Foroughi <ehsan@gmail.com>
2013-08-09 12:00:48 -0400

    fixed commit issue

--- fortify/fortify.py
+++ fortify/fortify.py
@@ -17,7 +17,6 @@ client = suds.client.Client("http://127.0.0.1:8181/ssc/fm-ws/services/fws.wsdl",
     location="http://127.0.0.1:8181/ssc/fm-ws/services/",
     wsse=security)
 ret = client.service.CreateAuditSession()
-<<<<<<< HEAD
 if ('code' not in ret) or (ret['code'] != 0):
     raise Exception('Error!')
 #ret = client.service.ProjectList()
@@ -33,19 +32,16 @@ ret = client.last_received()
 token = ret.getChild('Envelope').getChild('Body').getChild('GetAuthenticationTokenResponse').getChild('token').text
 print active_project_version_list
 print 'Token: %s' % token
-=======
-import pdb; pdb.set_trace()
 
-spec = []
-for method in client.wsdl.services[0].ports[0].methods.values(): 
-    if not method.soap.input.body.parts:
-        args = ''
-    else:
-        args = str(client.factory.create(method.soap.input.body.parts[0].element[0]))
-    spec.append((method.name, args))
-spec.sort()
-for item in spec:
-    print item[0]
-    print '\n    '.join(item[1].split('\n'))
-    print
->>>>>>> 49f0f77238e08f0ce7460037615b5b1ff029580f
+#spec = []
+#for method in client.wsdl.services[0].ports[0].methods.values(): 
+#    if not method.soap.input.body.parts:
+#        args = ''
+#    else:
+#        args = str(client.factory.create(method.soap.input.body.parts[0].element[0]))
+#    spec.append((method.name, args))
+#spec.sort()
+#for item in spec:
+#    print item[0]
+#    print '\n    '.join(item[1].split('\n'))
+#    print
