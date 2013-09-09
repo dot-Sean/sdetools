7233ce61e05a9c45491133eeffd47053e1b0da75
Kelvin Chan <kelvin@securitycompass.com>
2013-09-09 13:38:23 -0400

    Fixed indentations and removed whitespaces

--- sdetools/modules/sync_github/github_plugin.py
+++ sdetools/modules/sync_github/github_plugin.py
@@ -1,7 +1,6 @@
 # Copyright SDElements Inc
 # Extensible two way integration with GitHub
 
-import urllib
 from datetime import datetime
 
 from sdetools.extlib import http_req
@@ -10,10 +9,10 @@ from sdetools.sdelib.restclient import RESTBase
 from sdetools.sdelib.restclient import URLRequest, APIError
 from sdetools.alm_integration.alm_plugin_base import AlmTask, AlmConnector
 from sdetools.alm_integration.alm_plugin_base import AlmException
-from sdetools.sdelib.conf_mgr import Config
 
 from sdetools.sdelib import log_mgr
 logger = log_mgr.mods.add_mod(__name__)
+
 
 class GitHubAPI(RESTBase):
     """ Base plugin for GitHub """
@@ -21,7 +20,7 @@ class GitHubAPI(RESTBase):
     def __init__(self, config):
         super(GitHubAPI, self).__init__('alm', 'GitHub', config, '')
         self.proxy = None
-        
+
     def post_conf_init(self):
         urllib_debuglevel = 0
         if __name__ in self.config['debug_mods']:
@@ -34,9 +33,10 @@ class GitHubAPI(RESTBase):
         self.config['%s_server' % (self.conf_prefix)] = self.opener.server
 
         self.session_info = None
-        self.server = self._get_conf('server') 
+        self.server = self._get_conf('server')
         self.base_uri = '%s://%s' % (self._get_conf('method'), self.server)
-           
+
+
 class GitHubTask(AlmTask):
     """ Representation of a task in GitHub"""
 
@@ -65,9 +65,9 @@ class GitHubTask(AlmTask):
 
     def get_timestamp(self):
         """ Returns a datetime object """
-        return datetime.strptime(self.timestamp,
-                                 '%Y-%m-%dT%H:%M:%SZ')
-                                 
+        return datetime.strptime(self.timestamp, '%Y-%m-%dT%H:%M:%SZ')
+
+
 class GitHubConnector(AlmConnector):
     alm_name = 'GitHub'
     alm_card_type = 'github_issue_type'
@@ -80,28 +80,29 @@ class GitHubConnector(AlmConnector):
         """ Initializes connection to GitHub """
         super(GitHubConnector, self).__init__(config, alm_plugin)
 
-        config.add_custom_option(self.alm_card_type, 'Issue type represented by labels on GitHub',
-                default='')
-        config.add_custom_option(self.alm_new_status, 'Status to set for new tasks in GitHub',
-            default='open')
-        config.add_custom_option(self.alm_done_statuses, 'Statuses that signify a task is Done in GitHub',
-            default='closed')
-        config.add_custom_option(self.github_duplicate_label, 'GitHub label for duplicate issues',
-            default='duplicate')
+        config.add_custom_option(self.alm_card_type, 'Issue type represented'
+                                 'by labels on GitHub', default='')
+        config.add_custom_option(self.alm_new_status, 'Status to set for new'
+                                 'tasks in GitHub', default='open')
+        config.add_custom_option(self.alm_done_statuses, 'Statuses that '
+                                 'signify a task is Done in GitHub',
+                                 default='closed')
+        config.add_custom_option(self.github_duplicate_label, 'GitHub label'
+                                 'for duplicate issues', default='duplicate')
         config.add_custom_option(self.alm_project_version, 'GitHub milestone',
-            default='')
+                                 default='')
         self.alm_task_title_prefix = 'SDE '
-        
+
     def initialize(self):
         super(GitHubConnector, self).initialize()
 
         #Verify that the configuration options are set properly
-        for item in [self.alm_new_status, self.alm_done_statuses, self.github_duplicate_label]:
+        for item in [self.alm_new_status, self.alm_done_statuses,
+                     self.github_duplicate_label]:
             if not self.config[item]:
                 raise AlmException('Missing %s in configuration' % item)
 
-        self.config[self.alm_done_statuses] = (
-                self.config[self.alm_done_statuses].split(','))
+        self.config[self.alm_done_statuses] = (self.config[self.alm_done_statuses].split(','))
 
     def alm_connect_server(self):
         """ Verifies that GitHub connection works """
@@ -109,12 +110,14 @@ class GitHubConnector(AlmConnector):
         try:
             user_info = self.alm_plugin.call_api('user')
         except APIError, err:
-            raise AlmException('Unable to connect to GitHub service (Check server URL, '
-                    'user, pass). Reason: %s' % str(err))
-                    
+            raise AlmException('Unable to connect to GitHub service (Check'
+                               'server URL, user, pass). Reason: %s' %
+                               str(err))
+
         if user_info.get('message'):
-            raise AlmException('Could not authenticate GitHub user %s: %s' % (self.config['alm_user'], user_info['message']))
-            
+            raise AlmException('Could not authenticate GitHub user %s: %s' %
+                              (self.config['alm_user'], user_info['message']))
+
     def alm_connect_project(self):
         """ Verifies that the GitHub repo exists """
         self.project_uri = self.config['alm_project']
@@ -124,144 +127,165 @@ class GitHubConnector(AlmConnector):
             repo_info = self.alm_plugin.call_api('repos/%s' % self.project_uri)
         except APIError, err:
             raise AlmException('Unable to find GitHub repo. Reason: %s' % err)
-           
+
         if repo_info.get('message'):
-            raise AlmException('Error accessing GitHub repository %s: %s' % self.project_uri, repo_info['message'])
-    
+            raise AlmException('Error accessing GitHub repository %s: %s' %
+                               self.project_uri, repo_info['message'])
+
     def github_get_milestone_id(self, milestone_name):
         if not milestone_name:
             return None
 
         try:
-            milestone_list = self.alm_plugin.call_api('repos/%s/milestones' % self.project_uri)
+            milestone_list = self.alm_plugin.call_api('repos/%s/milestones' %
+                                                      self.project_uri)
         except APIError, err:
             logger.error(err)
-            raise AlmException('Unable to get milestone %s from GitHub' % milestone_name)
-    
+            raise AlmException('Unable to get milestone %s from GitHub' %
+                               milestone_name)
+
         for milestone in milestone_list:
             if milestone['title'] == milestone_name:
                 return milestone['number']
 
-        raise AlmException('Unable to find milestone %s from GitHub' % milestone_name)
-            
+        raise AlmException('Unable to find milestone %s from GitHub' %
+                           milestone_name)
+
     def alm_get_task(self, task):
         task_id = task['title']
         milestone_name = self.config[self.alm_project_version]
-        
+
         try:
             # We need to perform 2 API calls to search open and closed issues
-            open_issues = self.alm_plugin.call_api('legacy/issues/search/%s/%s/%s' % 
-                            (self.project_uri, self.config[self.alm_new_status], urlencode_str(task_id)))
-            closed_issues = self.alm_plugin.call_api('legacy/issues/search/%s/%s/%s' % 
-                            (self.project_uri, self.config[self.alm_done_statuses][0], urlencode_str(task_id)))
+            open_issues = self.alm_plugin.call_api('legacy/issues/search/%s/%s/%s' %
+                                                   (self.project_uri,
+                                                    self.config[self.alm_new_status],
+                                                    urlencode_str(task_id)))
+            closed_issues = self.alm_plugin.call_api('legacy/issues/search/%s/%s/%s' %
+                                                     (self.project_uri,
+                                                      self.config[self.alm_done_statuses][0],
+                                                      urlencode_str(task_id)))
             issues_list = open_issues['issues']
             issues_list.extend(closed_issues['issues'])
         except APIError, err:
             logger.error(err)
             raise AlmException('Unable to get task %s from GitHub' % task_id)
-        
+
         if (not issues_list):
             return None
-        
+
         if len(issues_list) > 1:
             index = 0
-            
+
             while index < len(issues_list):
                 issue = issues_list[index]
                 duplicate_label = self.config[self.github_duplicate_label]
-                
+
                 if issue['labels'].count(duplicate_label) > 0:
                     issues_list.pop(index)
                 else:
                     index = index + 1
-                            
+
             if len(issues_list) > 1:
-                raise AlmException('Found multiple issues with the title %s in milestone %s that are not labeled as duplicates' % (task_id, milestone_name))
+                raise AlmException('Found multiple issues with the title %s'
+                                   'in milestone %s that are not labeled as'
+                                   'duplicates' % (task_id, milestone_name))
             elif not issues_list:
                 return None
-                
+
         logger.info('Found task: %s', task_id)
         return GitHubTask(task_id,
-                              issues_list[0]['number'],
-                              issues_list[0]['state'],
-                              issues_list[0]['updated_at'],
-                              self.config[self.alm_done_statuses])        
+                          issues_list[0]['number'],
+                          issues_list[0]['state'],
+                          issues_list[0]['updated_at'],
+                          self.config[self.alm_done_statuses])
 
     def alm_add_task(self, task):
         milestone_name = self.config[self.alm_project_version]
 
         try:
-            create_args = { 
+            create_args = {
                 'title': task['title'],
                 'body': self.sde_get_task_content(task),
                 'labels': [self.config[self.alm_card_type]],
             }
-            
+
             milestone_id = self.github_get_milestone_id(milestone_name)
+
             if milestone_id:
                 create_args['milestone'] = milestone_id
 
-            new_issue = self.alm_plugin.call_api('repos/%s/issues' % self.project_uri,
-                    method = self.alm_plugin.URLRequest.POST, args = create_args)
+            new_issue = self.alm_plugin.call_api('repos/%s/issues' %
+                                                 self.project_uri,
+                                                 method=self.alm_plugin.URLRequest.POST,
+                                                 args=create_args)
             logger.debug('Task %s added to GitHub Project', task['id'])
         except APIError, err:
-            raise AlmException('Unable to add task %s to GitHub because of %s' % 
-                    (task['id'], err))
-        
+            raise AlmException('Unable to add task %s to GitHub because of %s'
+                               % (task['id'], err))
+
         if new_issue.get('errors'):
-            raise AlmException('Unable to add task GitHub to %s. Reason: %s - %s' % 
-                    (task['id'], str(new_issue['errors']['code']), 
-                    str(new_issue['errors']['field'])))
+            raise AlmException('Unable to add task GitHub to %s. Reason: %s - %s'
+                               % (task['id'], str(new_issue['errors']['code']),
+                                  str(new_issue['errors']['field'])))
 
         # API returns JSON of the new issue
         alm_task = GitHubTask(task['title'],
                               new_issue['number'],
                               new_issue['state'],
                               new_issue['updated_at'],
-                              self.config[self.alm_done_statuses])        
+                              self.config[self.alm_done_statuses])
 
-        if (self.config['alm_standard_workflow'] and (task['status'] == 'DONE' or
-                task['status'] == 'NA')):
+        if (self.config['alm_standard_workflow'] and
+                (task['status'] == 'DONE' or task['status'] == 'NA')):
             self.alm_update_task_status(alm_task, task['status'])
-            
+
         return 'Repository: %s, Issue: %s' % (self.config['alm_project'],
-                                           alm_task.get_alm_id())
-                                           
+                                              alm_task.get_alm_id())
+
     def alm_update_task_status(self, task, status):
         if not task or not self.config['alm_standard_workflow']:
             logger.debug('Status synchronization disabled')
             return
-            
-        if status == 'DONE' or status=='NA':
+
+        if status == 'DONE' or status == 'NA':
             try:
                 update_args = {
-                    'state':self.config[self.alm_done_statuses][0]
+                    'state': self.config[self.alm_done_statuses][0]
                 }
-                result = self.alm_plugin.call_api('repos/%s/issues/%s' % (self.project_uri, task.get_alm_id()),
-                        args=update_args, method=URLRequest.POST)
+                result = self.alm_plugin.call_api('repos/%s/issues/%s' %
+                                                  (self.project_uri,
+                                                   task.get_alm_id()),
+                                                  args=update_args,
+                                                  method=URLRequest.POST)
             except APIError, err:
                 raise AlmException('Unable to update task status to DONE '
                                    'for issue: %s in GitHub because of %s' %
                                    (task.get_alm_id(), err))
-        elif status== 'TODO':
+        elif status == 'TODO':
             try:
                 update_args = {
-                    'state':self.config[self.alm_new_status]
+                    'state': self.config[self.alm_new_status]
                 }
-                result = self.alm_plugin.call_api('repos/%s/issues/%s' % (self.project_uri, task.get_alm_id()),
-                        args=update_args, method=URLRequest.PUT)
+                result = self.alm_plugin.call_api('repos/%s/issues/%s' %
+                                                  (self.project_uri,
+                                                   task.get_alm_id()),
+                                                  args=update_args,
+                                                  method=URLRequest.PUT)
             except APIError, err:
                 raise AlmException('Unable to update task status to TODO for '
                                    'issue: %s in GitHub because of %s' %
                                    (task.get_alm_id(), err))
-        
+
         if (result and result.get('errors')):
-            raise AlmException('Unable to update status of task %s to %s. Reason: %s - %s' % 
-                    (task['id'], status, str(new_issue['errors']['code']), 
-                    str(new_issue['errors']['field']))) 
-                    
+            raise AlmException('Unable to update status of task %s to %s.'
+                               'Reason: %s - %s' %
+                               (task['id'], status,
+                                str(result['errors']['code']),
+                                str(result['errors']['field'])))
+
         logger.debug('Status changed to %s for task %s in GitHub' %
-                (status, task.get_alm_id()))
-                
+                     (status, task.get_alm_id()))
+
     def alm_disconnect(self):
         pass
