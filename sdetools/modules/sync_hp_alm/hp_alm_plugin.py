d03fd61d233e0fbdd8f4484e07c2e831f843db35
Geoff Whittington <geoff@sdelements.com>
2013-09-09 00:43:37 -0400

    Task priority is working, rich content, and status syncs

--- sdetools/modules/sync_hp_alm/hp_alm_plugin.py
+++ sdetools/modules/sync_hp_alm/hp_alm_plugin.py
@@ -16,29 +16,20 @@ from sdetools.extlib import markdown
 from sdetools.sdelib import log_mgr
 logger = log_mgr.mods.add_mod(__name__)
 
-MAX_CONTENT_SIZE = 30000
+HPALM_PRIORITY_MAP = {
+    '10': '5-Urgent',
+    '8-9': '4-Very High',
+    '7': '3-High',
+    '4-6': '2-Medium',
+    '1-3': '1-Low',
+    }
 
-HPALM_HTML_CONVERT = [
-    ('<h1>', '<br><font size="5">'),
-    ('<h2>', '<br><font size="4">'),
-    ('<h3>', '<br><font size="3">'),
-    ('<h4>', '<br><font size="2">'),
-    ('<h5>', '<br><font size="2">'),
-    ('<h6>', '<br><font size="2">'),
-    (re.compile('</h[1-6]>'), '</font><br><br>'),
-    ('\n', ''),
-    ('"', '\"'),
-    ('<p>', ''),
-    ('</p>', '<br><br>'),
-    ('<pre><code>', '<span style="font-family: courier new,monospace;"><pre><code>'),
-    ('</code></pre>', '</code></pre></span>'),
-]
 
 class HPAlmAPIBase(RESTBase):
     """ Base plugin for HP Alm """
 
     def __init__(self, config):
-        super(HPAlmAPIBase, self).__init__('alm', 'HPAlm', config, 
+        super(HPAlmAPIBase, self).__init__('alm', 'HP Alm', config, 
                 'qcbin')
 
     def parse_response(self, result):
@@ -60,14 +51,8 @@ class HPAlmTask(AlmTask):
     def get_task_id(self):
         return self.task_id
 
-    def get_alm_task_ref(self):
-        return self.alm_task_ref
-
     def get_alm_id(self):
         return self.alm_id
-
-    def get_priority(self):
-        return None
 
     def get_status(self):
         """Translates HP Alm status into SDE status"""
@@ -91,7 +76,7 @@ class HPAlmConnector(AlmConnector):
         config.add_custom_option('hp_alm_issue_type', 'IDs for issues raised in HP Alm',
             default='3')
         config.add_custom_option('hp_alm_new_status', 'status to set for new tasks in HP Alm',
-            default='Not Covered')
+            default='Not Completed')
         config.add_custom_option('hp_alm_done_statuses', 'Statuses that signify a task is Done in HP Alm',
             default='Passed')
         config.add_custom_option('hp_alm_domain', 'Domain', default=None)
@@ -107,18 +92,13 @@ class HPAlmConnector(AlmConnector):
         self.config['hp_alm_done_statuses'] = (
                 self.config['hp_alm_done_statuses'].split(','))
 
-        self.project_ref = None
-        self.domain_ref = None
         self.COOKIE_LWSSO = None
 
         self.mark_down_converter = markdown.Markdown(safe_mode="escape")        
 
-    def carriage_return(self):
-        return '<br//>'
-
     def alm_connect_server(self):
-        """ Verifies that Rally connection works """
-        #Check to make sure that we can do a simple API call
+        """ Verifies that HP Alm connection works """
+        #Check to make sure that we can login
         try:
             self.alm_plugin.auth_mode = 'basic'
             self.alm_plugin.call_api('authentication-point/authenticate')
@@ -130,14 +110,22 @@ class HPAlmConnector(AlmConnector):
                     'user, pass). Reason: %s' % str(err))
 
     def alm_connect_project(self):
-        pass
+        headers = {'Cookie':'LWSSO_COOKIE_KEY=%s' % self.COOKIE_LWSSO,
+                   'Content-Type':'application/json',
+                   'Accept':'application/json'}
+  
+        try:
+            user = self.alm_plugin.call_api('rest/domains/%s/projects/%s/customization/users/%s' 
+                                    % (self.config['hp_alm_domain'], self.config['alm_project'], 
+                                       self.config['alm_user']), call_headers=headers)
+        except APIError, err:
+            raise AlmException('Unable to verify domain and project details: %s' % (err))
+        
+        if user['Name'] != self.config['alm_user']:
+            raise AlmException('Unable to verify domain and project details: %s' % (err))
 
     def alm_get_task(self, task):
-        task_id = None
-        task_search = re.search('^(\d+)-([^\d]+\d+)$', task['id'])
-        if task_search:
-            task_id = task_search.group(2)    
-        
+        task_id = self._extract_task_id(task['id'])  
         if not task_id:
             return None
 
@@ -183,27 +171,34 @@ class HPAlmConnector(AlmConnector):
                          hp_alm_status,
                          hp_alm_last_modified,
                          self.config['hp_alm_done_statuses'])
-                         
-    def alm_add_task(self, task):
+                   
+    def _extract_task_id(self, full_task_id):
         task_id = None
-        task_search = re.search('^(\d+)-([^\d]+\d+)$', task['id'])
+        task_search = re.search('^(\d+)-([^\d]+\d+)$', full_task_id)
         if task_search:
             task_id = task_search.group(2)    
-        
+        return task_id
+
+    def alm_add_task(self, task):
+        task_id = self._extract_task_id(task['id'])  
         if not task_id:
             return None
+
+        task['formatted_content'] = json.dumps(self.sde_get_task_content(task))
+        task['alm_priority'] = self.translate_priority(task['priority'])
+        task['title'] = json.dumps(task['title'])
 
         # HP Alm is very particular about JSON ordering
         query_args = """{
             "Fields":[
                 {"Name":"type-id","values":[{"value":"3"}]},
-                {"Name":"status","values":[{"value":"Not Covered"}]},
-                {"Name":"name","values":[{"value":"%s"}]},
-                {"Name":"description","values":[{"value":"%s"}]},
-                {"Name":"req-priority","values":[{"value":"5-Urgent"}]}
+                {"Name":"status","values":[{"value":"%s"}]},
+                {"Name":"name","values":[{"value":%s}]},
+                {"Name":"description","values":[{"value":%s}]},
+                {"Name":"req-priority","values":[{"value":"%s"}]}
             ], 
             "Type":"requirement"
-        }""" % (task['title'], self.sde_get_task_content(task))
+        }""" % (self.config['hp_alm_new_status'], task['title'], task['formatted_content'], task['alm_priority'])
         try:
             headers = {'Cookie':'LWSSO_COOKIE_KEY=%s' % self.COOKIE_LWSSO,
                        'Content-Type':'application/json',
@@ -225,7 +220,6 @@ class HPAlmConnector(AlmConnector):
         
         return "Req ID %s" % alm_task.get_alm_id()
 
-
     def alm_update_task_status(self, task, status):
 
         if not task or not self.config['alm_standard_workflow']:
@@ -237,81 +231,63 @@ class HPAlmConnector(AlmConnector):
                    'Accept':'application/json'}
                    
         if status == 'DONE' or status == 'NA':
-            try:
-                query_args = """
-                    {"entities":[{"Fields":[
-                    {"Name":"id","values":[{"value":"%s"}]},
-                    {"Name":"status","values":[{"value":"Passed"}]}
-                    ]}]}""" % task.get_alm_id()
-
-                self.alm_plugin.call_api('rest/domains/%s/projects/%s/requirements' 
-                                        % (self.config['hp_alm_domain'], self.config['alm_project']),
-                                          args = query_args,
-                                          method=self.alm_plugin.URLRequest.PUT,
-                                          call_headers=headers)
-            except APIError, err:
-                raise AlmException('Unable to update task status to DONE '
-                                   'for card: %s in HP Alm because of %s' % 
-                                   (task.get_alm_id(), err))
+            json = """
+                {"entities":[{"Fields":[
+                {"Name":"id","values":[{"value":"%s"}]},
+                {"Name":"status","values":[{"value":"%s"}]}
+                ]}]}""" % (task.get_alm_id(), self.config['hp_alm_done_statuses'][0])
+            status = "DONE"
 
         elif status == 'TODO':
-            try:
-                query_args = """
-                    {"entities":[{"Fields":[
-                    {"Name":"id","values":[{"value":"%s"}]},
-                    {"Name":"status","values":[{"value":"Not Covered"}]}
-                    ]}]}""" % task.get_alm_id()
-                self.alm_plugin.call_api('rest/domains/%s/projects/%s/requirements' 
-                                        % (self.config['hp_alm_domain'], self.config['alm_project']),
-                                          args = query_args,
-                                          method=self.alm_plugin.URLRequest.PUT,
-                                          call_headers=headers)
-            except APIError, err:
-                raise AlmException('Unable to update task status to TODO '
-                                   'for card: '
-                                   '%s in HP Alm because of %s' %
-                                   (task.get_alm_id(), err))
+            json = """
+                {"entities":[{"Fields":[
+                {"Name":"id","values":[{"value":"%s"}]},
+                {"Name":"status","values":[{"value":"%s"}]}
+                ]}]}""" % (task.get_alm_id(), self.config['hp_alm_new_status'])
+        else:
+            raise AlmException('Unexpected status %s: valid values are DONE and TODO' % status)
+
+        try:
+            self.alm_plugin.call_api('rest/domains/%s/projects/%s/requirements' 
+                                    % (self.config['hp_alm_domain'], self.config['alm_project']),
+                                      args = json,
+                                      method=self.alm_plugin.URLRequest.PUT,
+                                      call_headers=headers)
+        except APIError, err:
+            raise AlmException('Unable to update task status to %s '
+                               'for requirement: '
+                               '%s in HP Alm because of %s' %
+                               (status, task.get_alm_id(), err))
 
         logger.debug('Status changed to %s for task %s in HP Alm',
                       status, task.get_alm_id())
 
     def alm_disconnect(self):
-        pass
-
-    def sde_get_task_content(self, task):
-        """ Convenience method that returns the text that should go into
-        content of an ALM ticket/defect/story for a given task.
-
-        Raises an AlmException on encountering an error
-
-        Keyword arguments:
-        task  -- An SD Elements task representing the task to enter in the
-                 ALM
-        """
-        content = '%s\n\nImported from SD Elements' % (task['content'])
-        return self.convert_markdown_to_alm(content, ref=task['id'])
+        try:
+            headers = {'Cookie':'LWSSO_COOKIE_KEY=%s' % self.COOKIE_LWSSO}
+            result = self.alm_plugin.call_api('authentication-point/logout', call_headers=headers)
+        except APIError, err:
+            raise AlmException('Unable to logout from HP Alm. Reason: %s' % (str(err)))
 
     def convert_markdown_to_alm(self, content, ref): 
-        s = self.mark_down_converter.convert(content)
+        return '<html>'+self.mark_down_converter.convert(content)+'</html>'
 
-        # We do some jumping through hoops to add <br> at end of each
-        # line for segments between code tags
-        sliced = s.split('<code>')
-        s = [sliced[0]]
-        for item in sliced[1:]:
-            item = item.split('</code>')
-            item[0] = item[0].replace('\n', '<br>\n')
-            s.append('</code>'.join(item))
-        s = '<code>'.join(s)
-
-        for before, after in HPALM_HTML_CONVERT:
-            if type(before) is str:
-                s = s.replace(before, after)
+    def translate_priority(self, priority):
+        """ Translates an SDE priority into a HP ALM priority """
+        try:
+            priority = int(priority)
+        except (TypeError):
+            logger.error('Could not coerce %s into an integer' % priority)
+            raise AlmException("Error in translating SDE priority to HP Alm: "
+                               "%s is not an integer priority" % priority)
+        pmap = HPALM_PRIORITY_MAP
+        for key in pmap:
+            if '-' in key:
+                lrange, hrange = key.split('-')
+                lrange = int(lrange)
+                hrange = int(hrange)
+                if lrange <= priority <= hrange:
+                    return pmap[key]
             else:
-                s = before.sub(after, s)
-
-        if len(s) > MAX_CONTENT_SIZE:
-            logger.warning('Content too long for %s - Truncating.' % ref)
-            s = s[:MAX_CONTENT_SIZE]
-
-        return s.replace('"','\"')
+                if int(key) == priority:
+                    return pmap[key]
