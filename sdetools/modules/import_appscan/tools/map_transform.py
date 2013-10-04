39dad0c191817b6afe1b43079c4826eed5ca775a
Geoff Whittington <geoff@sdelements.com>
2013-10-04 09:51:03 -0400

    preliminary commit

--- sdetools/modules/import_appscan/tools/map_transform.py
+++ sdetools/modules/import_appscan/tools/map_transform.py
@@ -32,11 +32,34 @@ class Mapping:
 
         self.mapping = category_mapping
 
+    def load_custom_mapping_from_xml(self, mapping_file):
+        try:
+            base = minidom.parse(mapping_file)
+        except Exception, e:
+            raise MappingError("An error occurred opening mapping file '%s': %s" % (mapping_file, e))
+
+        for task in base.getElementsByTagName('task'):
+            task_id = task.attributes['id'].value
+            task_checks = []
+            if task_id in self.task_map.keys():
+                task_checks = self.task_map[task_id]
+            for check_row in task.getElementsByTagName('weakness'):
+                check = {}
+                check['check_id'] = check_row.attributes['id'].value
+                check['check_name'] = check_row.attributes['title'].value
+                check['check_name'] = check['check_name'].replace('"', '&quot;')
+                check['check_name'] = check['check_name'].replace('&', '&amp;')
+
+                if not self.check_in_list(task_checks, check['check_id']):
+                    task_checks.append(check)
+
+            self.task_map[task_id] = task_checks
+
     def load_weaknesses_from_xml(self, checks_file):
         try:
             base = minidom.parse(checks_file)
         except Exception, e:
-            raise MappingError("An error occured opening checks file '%s': %s" % (checks_file, e))
+            raise MappingError("An error occurred opening checks file '%s': %s" % (checks_file, e))
 
         category_checks = {}
         for check_row in base.getElementsByTagName('ROW'):
@@ -57,40 +80,59 @@ class Mapping:
         self.category_checks = category_checks
         
     def remap(self):
-        task_map = {}
 
-        task_map['193'] = [ {'check_id':'*', 'category_id':"", 'check_name': 'Unmapped Check'} ]
+        self.task_map['193'] = [{'check_id': '*', 'check_name': 'Unmapped Check'}]
 
-        keys = sorted(self.category_checks.iterkeys())
+        keys = sorted(self.cwe_checks.iterkeys())
         for cwe in keys:
             if self.mapping.has_key(cwe):
                 tasks = self.mapping[cwe]
                 for task in tasks:
                     task_checks = []
-                    if task_map.has_key(task):
-                        task_checks = task_map[task]
-                    for check in self.category_checks[cwe]:
-                        task_mapping_found = False
-                        for task_check in task_checks:
-                            if task_check['check_id'] == check['check_id']:
-                                task_mapping_found = True
-                        if not task_mapping_found:
-                            task_checks.append(check)
-                    task_map[task] = task_checks
-            else: #map to 193
+                    if task in self.task_map.keys():
+                        task_checks = self.task_map[task]
+                    for check in self.cwe_checks[cwe]:
+
+                        # If the check is assigned elsewhere do not put it in the catch-all task
+                        if self.check_mapped(self.task_map, check['check_id']):
+                            continue
+
+                        task_checks.append(check)
+
+                    self.task_map[task] = task_checks
+            else:  # map to 193
                     task_mapping_found = False
                     task_checks = []
                     task = '193'
-                    if task_map.has_key(task):
-                        task_checks = task_map[task]
-                    for check in self.category_checks[cwe]:
+                    if task in self.task_map.keys():
+                        task_checks = self.task_map[task]
+                    for check in self.cwe_checks[cwe]:
+
+                        # If the check is assigned elsewhere do not put it in the catch-all task
+                        if self.check_mapped(self.task_map, check['check_id']):
+                            continue
+
                         for chk in task_checks:
                             if chk['check_id'] == check['check_id']:
                                 task_mapping_found = True
                         if not task_mapping_found:
                             task_checks.append(check)
-                    task_map[task] = task_checks
-        self.task_map = task_map
+                    self.task_map[task] = task_checks
+
+    def check_in_list(self, a_list, check_id):
+        for item in a_list:
+            if item['check_id'] == check_id:
+                return True
+        return False
+
+    def check_mapped(self, task_map, check_id):
+        keys = task_map.iterkeys()
+
+        for task_id in keys:
+            task_checks = task_map[task_id]
+            if self.check_in_list(task_checks, check_id):
+                return True
+        return False
 
     def output_mapping(self, file_name):
         fp = open(file_name, "w")
@@ -128,11 +170,12 @@ class Mapping:
 
 def main(argv):
 
-    usage = "Usage: %s <category mapping> <appscan checks> <appscan map>\n" % (argv[0])
+    usage = "Usage: %s <category mapping> <custom map> <appscan checks> <appscan map>\n" % (argv[0])
 
     if len(argv) == 2 and argv[1] == 'help':
         print usage
         print "category mapping: (input) docs/appscan/sde_appscan_standard_category_map.xml"
+        print "custom map: (input) Task->WebInspect check mapping in xml format [i.e. custom_webinspect_map.xml]"
         print "appscan checks: (input) AppScan checks in xml format [i.e. appscan_checks.xml]"
         print "appscan map: (output) AppScan map file"
         sys.exit(0)
@@ -142,11 +185,14 @@ def main(argv):
 
     base = Mapping()
 
-    print "Loading Category map..."
+    print "Loading category map..."
     base.load_base_mapping_from_xml(argv[1])
     
-    print "Loading AppScan Checks..."    
-    base.load_weaknesses_from_xml(argv[2])
+    print "Loading custom map..."
+    base.load_custom_mapping_from_xml(argv[2])
+
+    print "Loading AppScan Checks..."
+    base.load_weaknesses_from_xml(argv[3])
     
     print "Re-mapping Checks..."
     base.remap()
@@ -155,9 +201,9 @@ def main(argv):
     if base.find_missing_checks():
         print "Aborted"
         sys.exit(1)
-        
-    print "Outputting AppScan map to %s..." % argv[3]
-    base.output_mapping(argv[3])
+
+    print "Outputting AppScan map to %s..." % argv[5]
+    base.output_mapping(argv[5])
 
     print "Done."        
 
