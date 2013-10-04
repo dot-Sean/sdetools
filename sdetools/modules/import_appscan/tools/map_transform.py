e0adc963ef25da040524c2463a7e0a47560475ce
Geoff Whittington <geoff@sdelements.com>
2013-10-04 10:51:13 -0400

    Getting closer to completion

--- sdetools/modules/import_appscan/tools/map_transform.py
+++ sdetools/modules/import_appscan/tools/map_transform.py
@@ -13,6 +13,7 @@ class Mapping:
         self.category_checks = {}
         self.base_tasks = {}
         self.checks = []
+        self.task_map = {}
 
     def load_base_mapping_from_xml(self, mapping_file):
         try:
@@ -24,11 +25,11 @@ class Mapping:
         for task in base.getElementsByTagName('task'):
             self.base_tasks[ task.attributes['id'].value ] = task
             for category in task.getElementsByTagName('category'):
-                 tasks = []
-                 if (category_mapping.has_key(category.attributes['id'].value)):
-                     tasks = category_mapping[category.attributes['id'].value]
-                 tasks.append(task.attributes['id'].value)
-                 category_mapping[category.attributes['id'].value] = tasks
+                tasks = []
+                if category_mapping.has_key(category.attributes['id'].value):
+                    tasks = category_mapping[category.attributes['id'].value]
+                    tasks.append(task.attributes['id'].value)
+                category_mapping[category.attributes['id'].value] = tasks
 
         self.mapping = category_mapping
 
@@ -67,9 +68,13 @@ class Mapping:
             check['check_id'] = check_row.attributes['ID'].value
             check['category_id'] = check_row.attributes['THREATCLASSID'].value
             check['check_name'] = check_row.attributes['ADVISORYID'].value
+            check['third_party'] = check_row.attributes['THIRDPARTY'].value == '1'
+
+            if check['third_party']:
+                self.task_map['186'].append(check)
 
             checks = []
-            if (category_checks.has_key(check['category_id'])):
+            if category_checks.has_key(check['category_id']):
                 checks = category_checks[check['category_id']]
             checks.append(check)
             category_checks[check['category_id']] = checks
@@ -78,12 +83,12 @@ class Mapping:
                 self.checks.append(check['check_id'])
             
         self.category_checks = category_checks
-        
+
     def remap(self):
 
         self.task_map['193'] = [{'check_id': '*', 'check_name': 'Unmapped Check'}]
 
-        keys = sorted(self.cwe_checks.iterkeys())
+        keys = sorted(self.category_checks.iterkeys())
         for cwe in keys:
             if self.mapping.has_key(cwe):
                 tasks = self.mapping[cwe]
@@ -91,7 +96,7 @@ class Mapping:
                     task_checks = []
                     if task in self.task_map.keys():
                         task_checks = self.task_map[task]
-                    for check in self.cwe_checks[cwe]:
+                    for check in self.category_checks[cwe]:
 
                         # If the check is assigned elsewhere do not put it in the catch-all task
                         if self.check_mapped(self.task_map, check['check_id']):
@@ -106,7 +111,7 @@ class Mapping:
                     task = '193'
                     if task in self.task_map.keys():
                         task_checks = self.task_map[task]
-                    for check in self.cwe_checks[cwe]:
+                    for check in self.category_checks[cwe]:
 
                         # If the check is assigned elsewhere do not put it in the catch-all task
                         if self.check_mapped(self.task_map, check['check_id']):
@@ -141,9 +146,10 @@ class Mapping:
         for task_id in keys:
             task_checks = self.task_map[task_id]
             confidence = "low"
+            """
             if 'confidence' in self.base_tasks[task_id].attributes.keys():
                 confidence = self.base_tasks[task_id].attributes['confidence'].value
-
+            """
             fp.write('\t<task id="%s" title="%s" confidence="%s">\n'%
                     (task_id, self.base_tasks[task_id].attributes['title'].value, confidence))
             task_checks = sorted(task_checks)
@@ -154,16 +160,9 @@ class Mapping:
         fp.close()
 
     def find_missing_checks(self):
-        keys = sorted(self.task_map.iterkeys())
         missing_checks = False
         for check in self.checks:
-            check_found = False
-            for task_id in keys:
-                task_checks = self.task_map[task_id]
-                for task_check in task_checks:
-                    if check == task_check['check_id']:
-                         check_found = True
-            if not check_found:
+            if not self.check_mapped(self.task_map, check):
                 print "*** Check %s is not mapped" % check
                 missing_checks = True
         return missing_checks
@@ -202,8 +201,8 @@ def main(argv):
         print "Aborted"
         sys.exit(1)
 
-    print "Outputting AppScan map to %s..." % argv[5]
-    base.output_mapping(argv[5])
+    print "Outputting AppScan map to %s..." % argv[4]
+    base.output_mapping(argv[4])
 
     print "Done."        
 
