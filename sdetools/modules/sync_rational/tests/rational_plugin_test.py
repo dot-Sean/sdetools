898ceaafe138de703cdcb55406a0faf1b271134d
Timur Vataev <timur@securitycompass.com>
2014-02-24 09:22:02 -0500

    cleaning the code according to git comments.

--- sdetools/modules/sync_rational/tests/rational_plugin_test.py
+++ sdetools/modules/sync_rational/tests/rational_plugin_test.py
@@ -23,7 +23,6 @@ class TestRationalCase(AlmPluginTestBase):
             raise AssertionError(error_msg)
 
     def test_update_task_status_to_done(self):
-        """TEST: Update SDE Task Status to DONE"""
 
         self.connector.config['conflict_policy'] = 'alm'
         self.connector.config['alm_phases'] = ['requirements', 'testing', 'development']
@@ -65,52 +64,3 @@ class TestRationalCase(AlmPluginTestBase):
         self.assertEqual(test_task['id'][test_task['id'].find('T'):], alm_task.get_task_id(), 'Files don\'t match, mismatch: %s - %s' % (test_task['id'][test_task['id'].find('T'):], alm_task.get_task_id()))
         self.assertEqual(test_task['status'], alm_task.get_status(), 'Files don\'t match, mismatch: %s - %s' % (test_task['status'], alm_task.get_status()))
 
-
-"""
-    def test_parsing_alm_task(self):
-        result = super(TestRationalCase, self).test_parsing_alm_task()
-        test_task = result[0]
-        test_task_result = result[1]
-        alm_id = test_task['id'].split('T')[1]
-        result_alm_id = test_task_result.get_alm_id()
-
-        self.assertEqual(result_alm_id, alm_id, 'Expected alm_id %s, got %s' % (alm_id, result_alm_id))
-
-
-    def test_parse_non_done_status_as_todo(self):
-        self.connector.alm_connect()
-        test_task = self.mock_sde_response.generate_sde_task()
-        self.connector.alm_add_task(test_task)
-        test_task_result = self.connector.alm_get_task(test_task)
-        test_task_result.status = "Non-done status"
-
-
-        self.assertEqual(test_task_result.get_status(), "TODO", 'Expected status to default to TODO')
-
-    def test_invalid_api_token(self):
-        self.mock_alm_response.set_response_flags({'get_user': '401'})
-        self.connector.config['alm_api_token'] = 'testApiToken'
-
-        exception_msg = 'Unable to connect to GitHub service (Check server URL, api token). Reason: '\
-                        'HTTP Error 401. Explanation returned: FATAL ERROR: Requires authentication'
-
-        self.assert_exception(AlmException, '', exception_msg, self.connector.alm_connect_server)
-
-    def test_invalid_user_pass(self):
-        self.mock_alm_response.set_response_flags({'get_user': '401'})
-        self.connector.config['alm_api_token'] = ''
-        exception_msg = 'Unable to connect to GitHub service (Check server URL, user, pass). Reason: '\
-                        'HTTP Error 401. Explanation returned: FATAL ERROR: Requires authentication'
-
-        self.assert_exception(AlmException, '', exception_msg, self.connector.alm_connect_server)
-
-    def test_invalid_field(self):
-        self.mock_alm_response.set_response_flags({'post_issue': '422'})
-        self.connector.alm_connect()
-        test_task = self.mock_sde_response.generate_sde_task()
-        exception_msg = 'Unable to add task %s to GitHub. Reason: HTTP Error 422. Explanation returned: FATAL ERROR: Validation Failed. ' \
-                        'Additional Info - The field "title" is required for the resource "Issue"' % test_task['id']
-
-        self.assert_exception(AlmException, '', exception_msg, self.connector.alm_add_task, test_task)
-
-"""
--- sdetools/modules/sync_rational/tests/rational_response_generator.py
+++ sdetools/modules/sync_rational/tests/rational_response_generator.py
@@ -9,12 +9,12 @@ class RationalResponseGenerator(ResponseGenerator):
         rest_api_targets = {
             '.*/rootservices': 'get_rootservices',
             '.*/oslc/workitems/catalog': 'get_catalog',
-            '.*/oslc/contexts/_gkmCkniWEeOI1P1pr3yq5Q/workitems/services': 'get_services',
-            '.*/oslc/context/_gkmCkniWEeOI1P1pr3yq5Q/shapes/workitems/task': 'get_resourceshape',
-            '.*/oslc/enumerations/_gkmCkniWEeOI1P1pr3yq5Q/priority/priority.literal.l.*': 'get_priorities',
-            '.*/oslc/contexts/_gkmCkniWEeOI1P1pr3yq5Q/workitems/workitems\\?oslc.where=dcterms:title=.*': 'get_count',
+            '.*/oslc/contexts/[^/]+/workitems/services': 'get_services',
+            '.*/oslc/context/[^/]+/shapes/workitems/task': 'get_resourceshape',
+            '.*/oslc/enumerations/[^/]+/priority/priority.literal.l.*': 'get_priorities',
+            '.*/oslc/contexts/[^/]+/workitems/workitems\\?oslc.where=dcterms:title=.*': 'get_count',
             '.*/resource/itemName/com.ibm.team.workitem.WorkItem/\\d*': 'get_workitem',
-            '.*/oslc/contexts/_gkmCkniWEeOI1P1pr3yq5Q/workitems/task': 'post_workitem',
+            '.*/oslc/contexts/[^/]+/workitems/task': 'post_workitem',
         }
         super(RationalResponseGenerator, self).__init__(rest_api_targets, resource_templates, test_dir)
 
@@ -27,28 +27,6 @@ class RationalResponseGenerator(ResponseGenerator):
     def raise_error(self, error_code, message=None):
         """Raises an error based on the error code or message"""
 
-        if message is None:
-            if error_code == '401':
-                message = {
-                    "message": "Requires authentication",
-                }
-            elif error_code == '404':
-                message = {
-                    "message": "Not found",
-                }
-            elif error_code == '422':
-                message = {
-                    "message": "Validation Failed",
-                    "errors": [{
-                        "resource": "Issue",
-                        "field": "title",
-                        "code": "missing_field"
-                    }]
-                }
-            else:
-                message = {
-                    "message": "Error",
-                }
         super(RationalResponseGenerator, self).raise_error(error_code, message['message'])
 
     """
@@ -94,13 +72,11 @@ class RationalResponseGenerator(ResponseGenerator):
     def get_workitem(self, target, flag, data, method):
         if not flag:
             res = self.generator_get_all_resource('workitem')[0]
-            #print res
             return res
         else:
             self.raise_error('404')
 
     def post_workitem(self, target, flag, data, method):
-        #print method
         if not flag:
             res = self.generator_get_all_resource('workitem')[0]
             for x in data:
@@ -109,38 +85,3 @@ class RationalResponseGenerator(ResponseGenerator):
             return res
         else:
             self.raise_error('404')
-
-
-    def sde_app(self, target, flag, data, method):
-        if not flag:
-            return self.generator_get_all_resource('sde_app')[0]
-        else:
-            self.raise_error('404')
-
-    def sde_proj(self, target, flag, data, method):
-        if not flag:
-            return self.generator_get_all_resource('sde_proj')[0]
-        else:
-            self.raise_error('404')
-
-    def sde_tasks(self, target, flag, data, method):
-        if not flag:
-            return self.generator_get_all_resource('sde_task')[0]
-        else:
-            self.raise_error('404')
-
-    def sde_task(self, target, flag, data, method):
-        if method == 'PUT':
-            if not flag:
-                res = self.generator_get_all_resource('sde_task1')[0]
-                for x in data:
-                    res[x] = data[x]
-                self.generator_update_resource('sde_task1', '0', update_args=res)
-                return res
-            else:
-                self.raise_error('404')
-        else:
-            if not flag:
-                return self.generator_get_all_resource('sde_task1')[0]
-            else:
-                self.raise_error('404')
