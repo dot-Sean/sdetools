dd3b1d4e57ea750d2fb156af3d70f9c44a7bd220
Geoff Whittington <geoff@sdelements.com>
2014-02-28 13:06:50 -0500

    Live rational  testing

---
+++ sdetools/modules/sync_rational/tests/live/rational_plugin_live_test.py
@@ -0,0 +1,15 @@
+# NOTE: Before running ensure that the options are set properly in the
+#       configuration file
+import unittest
+
+from sdetools.alm_integration.tests.live.alm_plugin_live_test_base import AlmPluginLiveTestBase
+from sdetools.modules.sync_rational.rational_plugin import RationalConnector, RationalAPI
+
+
+class TestRationalLiveCase(AlmPluginLiveTestBase, unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        super(TestRationalLiveCase, self).setUpClass(RationalConnector, RationalAPI)
+
+    def test_synchronize_sde_as_master(self):
+        pass
