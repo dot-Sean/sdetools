33b69b3ed360e90c1d35687109ece198bb1a8593
Geoff Whittington <geoff@sdelements.com>
2014-03-03 06:06:40 -0500

    Created a workaround for case when config was not being read properly

--- sdetools/modules/sync_jira/tests/jira_plugin_live_soap_test.py
+++ sdetools/modules/sync_jira/tests/jira_plugin_live_soap_test.py
@@ -5,9 +5,14 @@ import unittest
 from sdetools.alm_integration.tests.alm_plugin_live_test_base import AlmPluginLiveTestBase
 from sdetools.modules.sync_jira.jira_plugin import JIRAConnector
 from sdetools.modules.sync_jira.jira_soap import JIRASoapAPI
+from sdetools.modules.sync_jira.jira_rest import JIRARestAPI
 
 
 class TestJIRASoapLiveCase(AlmPluginLiveTestBase, unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        super(TestJIRASoapLiveCase, self).setUpClass(JIRAConnector, JIRASoapAPI)
+        super(TestJIRASoapLiveCase, self).setUpClass(JIRAConnector, JIRARestAPI)
+
+    def setUp(self):
+        super(TestJIRASoapLiveCase, self).setUp()
+        self.connector.alm_plugin = JIRASoapAPI(self.config)
